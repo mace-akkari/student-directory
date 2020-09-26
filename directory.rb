57418a006f6392d1f7903a9a167354b7c42b311e
Mace <maceplus@gmail.com>
2020-09-27 00:50:14 +0100

    returned footer

--- directory.rb
+++ directory.rb
@@ -28,6 +28,10 @@ def print(students)
   end
 end
 
+def print_footer(names)
+  puts "Overall, we have #{names.count} great students"
+end
+
 students = input_students
 print_header
 print(students)
