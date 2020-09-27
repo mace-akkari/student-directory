aaafb493c74d2ba54ab92927e09b9e115dbfe970
Mace <maceplus@gmail.com>
2020-09-27 01:40:30 +0100

    upto ex.10

--- directory.rb
+++ directory.rb
@@ -1,21 +1,32 @@
-# let's put all students into an array
-def input_students 
+def input_students
   puts "Please enter the names of the students"
   puts "To finish, just hit return twice"
-  #an empty array
-  students = [] 
-  #get the first name
+  # Create an empty array
+  students = []
+  # get the first name
   name = gets.chomp
-  #while the name is not empty, repeat this code
-  while !name.empty? do 
-    #add the student hash to the array
-    students << {name: name, cohort: :november}
-    puts "Now we have #{students.count} students"
-    #get another name from the user
-    name = gets.chomp
-  end 
-    students 
-end 
+  # while the name is not empty, repeat this code
+  while !name.empty? do
+    # get cohort
+    puts "Enter cohort: "
+    cohort = gets.chomp
+    # add the student hash to the array
+    if cohort == ""
+      students << {name: name.capitalize, cohort: "TBC"}
+    else
+      students << {name: name.capitalize, cohort: cohort.capitalize.to_sym}
+    end
+    if students.count == 1
+      puts "Now we have #{students.count} student"
+    else
+      puts "Now we have #{students.count} students"
+    end
+    # get another name from the user
+    name = gets.gsub("\n", "")
+  end
+  # return the array of students
+  return students
+end
 
 def print_header
   puts "The students of Villains Academy"
@@ -24,9 +35,13 @@ end
 
 def print(students)
   students.each.with_index(1) do |student, index|
-    puts "#{index} #{student[:name]} (#{student[:cohort]} cohort)"
+    # (ex.2) if student[:name][0].downcase == "a"
+    # (.3) if student[:name].length < 12
+    puts "#{index}) #{student[:name]} (#{student[:cohort]} cohort)"
+    #end
   end
-end
+end 
+
 
 def print_footer(names)
   puts "Overall, we have #{names.count} great students"
